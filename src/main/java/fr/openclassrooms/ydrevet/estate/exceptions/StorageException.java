f1e8a32e0e0abe95aae761ea186e1239fec432e7
Yoann DREVET <yoann.drevet0@laposte.net>
2024-06-07 15:53:26 +0200

    Création d’un service de stockage de fichiers.

---
+++ src/main/java/fr/openclassrooms/ydrevet/estate/exceptions/StorageException.java
@@ -0,0 +1,14 @@
+package fr.openclassrooms.ydrevet.estate.exceptions;
+
+public class StorageException extends RuntimeException {
+    public StorageException() {
+    }
+
+    public StorageException(String message) {
+        super(message);
+    }
+
+    public StorageException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
