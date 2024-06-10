0b15fe6dc32eb565f72632db4b2671d4515366f6
Yoann DREVET <yoann.drevet0@laposte.net>
2024-06-10 10:53:33 +0200

    Configuration initiale d’OpenAPI

---
+++ src/main/java/fr/openclassrooms/ydrevet/estate/configuration/OpenApiConfiguration.java
@@ -0,0 +1,25 @@
+package fr.openclassrooms.ydrevet.estate.configuration;
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class OpenApiConfiguration {
+    @Bean
+    public OpenAPI openApiDocumentation() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("Estate API")
+                        .description("API pour l'application Estate"))
+                .components(new Components()
+                        .addSecuritySchemes("jwt", new SecurityScheme()
+                                .type(SecurityScheme.Type.HTTP)
+                                .scheme("bearer")
+                                .bearerFormat("JWT")));
+    }
+
+}
